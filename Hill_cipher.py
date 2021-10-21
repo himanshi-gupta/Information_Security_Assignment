0d02a15591ef11a7394c7c5c254b525920716514
himanshi-gupta <himanshigupta579@gmail.com>
2021-10-21 23:49:04 +0530

    second commit

--- Hill_cipher.py
+++ Hill_cipher.py
@@ -1,5 +1,13 @@
+'''Menu Driven program to implement encryption and decryption using hill cipher'''
 
 def encrypt_2(plain_text,key):
+    '''
+    Purpose of the function is to encrypt the even length plain text using 2x2 matrix.
+    
+    Input : plain_text - text to be encoded
+            key - 2x2 matrix used for encryption
+    Output : returns a cipher text after using hill cipher by applying key
+    '''
     result=[]
     for i in range(0,len(plain_text),2):
         for j in range(2):
@@ -8,6 +16,15 @@ def encrypt_2(plain_text,key):
     return cipher_text
 
 def encrypt(plain_text,key):
+    '''
+    Purpose of the function is to encrypt plaintext of any length. if length is odd, using
+    only key[0][0] to encrypt the plaintext last alphabet else invoking the encrypt_2()
+    to encrypt the even length plain text
+    
+    Input : plain_text - text to be encoded
+            key - 2x2 matrix used for encryption
+    Output : returns a cipher text after using hill cipher by applying key
+    '''
     rem=len(plain_text)%2
     if(rem==1):
         cipher=encrypt_2(plain_text[:-1],key)
@@ -17,22 +34,51 @@ def encrypt(plain_text,key):
     return cipher
         
 def mul_inverse(key):
+    '''
+    Purpose of the function is to return the multiplicative inverse of key mod 26
+    
+    Input : key - key whose inverse mod 26 need to be found
+    
+    Output : returns the inverse of key mod 26
+    '''
     for i in range(26):
         if (key*i)%26==1:
             return i
         
 def check_inverse(key):
+    '''
+    Purpose of the function is to check the inverse of matrix exist as if inverse doesnot exist
+    decryption can't be done
+    
+    Input : key - 2x2 matrix whose inverse need to be checked
+    Output : returns true if inverse of matrix is possible else false
+    '''
     det=(key[0][0]*key[1][1]-key[1][0]*key[0][1])%26
     if det in (1,3,5,7,9, 11, 15, 17, 19, 21, 23,25):
         return True
     return False
 
 def key_inverse(key,inv):
+    '''
+    Purpose of the function is to find the inverse of key(2x2 matrix) and return the inverse
+    key.
+    
+    Input : inv - multiplicative inverse of determinant
+            key - 2x2 matrix whose inverse need to be found 
+    Output : returns the inverse of key
+    '''
     key[0][0],key[1][1]=(key[1][1]%26)*inv%26,(key[0][0]%26)*inv%26
     key[0][1],key[1][0]=(-1*key[0][1]%26)*inv%26,(-1*key[1][0]%26)*inv%26
     return key
 
 def decrypt_2(cipher_text,key):
+    '''
+    Purpose of the function is to decrypt the cipher text of even length
+    
+    Input : cipher_text - text need to be decrypted
+            key - 2x2 decryption matrix
+    Output : returns the plain text for even length cipher text.
+    '''
     result=[]
     for i in range(0,len(cipher_text),2):
         for j in range(2):
@@ -41,6 +87,15 @@ def decrypt_2(cipher_text,key):
     return plain_text
 
 def decrypt(cipher_text,key):
+    '''
+    Purpose of the function is to decrypt the cipher text of any length.if length is odd, using
+    only key[0][0] to decrypt the cipher text last alphabet else invoking the encrypt_2()
+    to decrypt the even length cipher text
+    
+    Input : cipher_text - text to be decrypted
+            key - 2x2 decryption matrix
+    Output : returns the plain text for cipher text.
+    '''
     plain_text=""
     det=(key[0][0]*key[1][1]-key[1][0]*key[0][1])%26
     inv=mul_inverse(det)
@@ -93,6 +148,5 @@ def main():
         else:
             print("Invalid Input!")
            
-
 if __name__=='__main__':
     main()
